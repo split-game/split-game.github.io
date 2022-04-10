0e6d7c8faa7b98f2aa87246ea81551c35fdc7c07
split-game <103388494+split-game@users.noreply.github.com>
2022-04-11 02:58:08 +0400

    ver 0.1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 let numbers = Array.from(document.querySelectorAll(".number"));
 let splits = Array.from(document.querySelectorAll(".split"));
+let startButton = document.querySelector("#start-button");
+let players = Array.from(document.querySelectorAll(".player"));
+let turn = document.querySelector(".turn-active");
 
 function getNumber() {
   var num = Math.floor(Math.random() * 36);
 
   numbers.map((number) => {
     number.dataset.number == num.toString()
-      ? (number.style.background = "#c3aeae")
-      : (number.style.background = "transparent");
+      ? number.classList.add("number-active")
+      : number.classList.remove("number-active");
   });
+
+  startButton.setAttribute("disabled", true);
+  startButton.style.pointerEvents = "none";
+  startButton.style.opacity = "0.5";
 
   markSplits(splits, num);
 }
@@ -26,13 +33,38 @@ function markSplits(arr, number) {
 
   arr.map((split) => {
     if (split.dataset.check === "") {
-      split.style.background = "transparent";
-      split.style.border = "none";
+      split.classList.remove("active");
     }
   });
+
   splitArr.map((split) => {
-    split.style.background = "#0b9b117a";
-    split.style.borderStyle = "dashed";
-    split.style.borderColor = "darkgreen";
+    split.classList.add("active");
+    split.setAttribute("onclick", "checkSplit(this)");
+  });
+}
+
+function checkSplit(split) {
+  split.style.background = document.querySelector(".turn-active").dataset.color;
+  split.dataset.check = turn.id;
+  split.classList.add("checked");
+  splits.forEach((rem) => {
+    rem.removeAttribute("onclick");
+    rem.classList.remove("active");
+  });
+
+  numbers.map((number) => {
+    number.classList.remove("number-active");
+  });
+
+  startButton.removeAttribute("disabled");
+  startButton.style.pointerEvents = "all";
+  startButton.style.opacity = "1";
+
+  players.forEach((player) => {
+    if (player.classList.contains("turn-active")) {
+      player.classList.remove("turn-active");
+    } else {
+      player.classList.add("turn-active");
+    }
   });
 }

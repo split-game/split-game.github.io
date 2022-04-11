165988dc7f08f2d235c7120565c53c22a9079ddc
split-game <103388494+split-game@users.noreply.github.com>
2022-04-12 01:01:08 +0400

    v0.0.2

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@ let splits = Array.from(document.querySelectorAll(".split"));
 let startButton = document.querySelector("#start-button");
 let players = Array.from(document.querySelectorAll(".player"));
 let turn = document.querySelector(".turn-active");
+let main = document.getElementById("main-layout");
+let buttonLogo = document.querySelector("#button-logo");
+let straightups = document.querySelectorAll(".straight");
 
 function getNumber() {
   var num = Math.floor(Math.random() * 36);
+  // var num = 5;
 
   numbers.map((number) => {
     number.dataset.number == num.toString()
@@ -13,11 +17,18 @@ function getNumber() {
       : number.classList.remove("number-active");
   });
 
+  buttonLogo.style.backgroundColor =
+    document.querySelector(".turn-active").dataset.color;
   startButton.setAttribute("disabled", true);
   startButton.style.pointerEvents = "none";
-  startButton.style.opacity = "0.5";
 
   markSplits(splits, num);
+
+  if (+num >= 19) {
+    main.scrollTo({ top: main.scrollHeight, behavior: "smooth" });
+  } else {
+    main.scrollTo({ top: 0, behavior: "smooth" });
+  }
 }
 
 function markSplits(arr, number) {
@@ -37,20 +48,59 @@ function markSplits(arr, number) {
     }
   });
 
-  splitArr.map((split) => {
-    split.classList.add("active");
-    split.setAttribute("onclick", "checkSplit(this)");
-  });
+  if (splitArr.length === 0) {
+    players.forEach((player) => {
+      if (player.classList.contains("turn-active")) {
+        player.classList.remove("turn-active");
+      } else {
+        player.classList.add("turn-active");
+      }
+    });
+
+    startButton.removeAttribute("disabled");
+    startButton.style.pointerEvents = "all";
+  } else {
+    splitArr.map((split) => {
+      split.classList.add("active");
+      split.setAttribute("onclick", `checkSplit(this, ${number} )`);
+    });
+  }
 }
 
-function checkSplit(split) {
+function checkSplit(split, number) {
+  var matchingSplits = [];
+  var matchingNumber;
   split.style.background = document.querySelector(".turn-active").dataset.color;
-  split.dataset.check = turn.id;
+  split.dataset.check = document.querySelector(".turn-active").id;
+
+  console.log(document.querySelector(".turn-active").id);
+
   split.classList.add("checked");
   splits.forEach((rem) => {
     rem.removeAttribute("onclick");
     rem.classList.remove("active");
   });
+
+  numbers.forEach((numb) => {
+    if (+numb.dataset.number === +number) {
+      matchingNumber = numb;
+    }
+  });
+
+  splits.forEach((num) => {
+    if (
+      num.dataset.split.split("-").some((val) => val === number.toString()) &&
+      num.classList.contains("checked") &&
+      num.dataset.check === document.querySelector(".turn-active").id
+    ) {
+      matchingSplits.push(num);
+    }
+  });
+
+  if (+matchingNumber.dataset.amount === matchingSplits.length) {
+    matchingNumber.children[0].style.backgroundColor =
+      document.querySelector(".turn-active").dataset.color;
+  }
 
   numbers.map((number) => {
     number.classList.remove("number-active");
@@ -58,7 +108,6 @@ function checkSplit(split) {
 
   startButton.removeAttribute("disabled");
   startButton.style.pointerEvents = "all";
-  startButton.style.opacity = "1";
 
   players.forEach((player) => {
     if (player.classList.contains("turn-active")) {
